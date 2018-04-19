fd85a9c5483e369e995484f2e9979b554dfa8301
Mauricio Barcelo <lctr30@gmail.com>
2018-04-19 19:46:25 -0300

    + add options to SectionChooser

--- library/src/main/java/com/std28/lib/interfaces/SectionChooser.java
+++ library/src/main/java/com/std28/lib/interfaces/SectionChooser.java
@@ -1,5 +1,7 @@
 package com.std28.lib.interfaces;
 
+import android.os.Bundle;
+
 /**
  * Created by Mauricio Barcelo on 11/02/18.
  * email: mauricio.barcelo30@gmail.com
@@ -8,5 +10,11 @@ package com.std28.lib.interfaces;
 public interface SectionChooser {
 
     void goToSection(int sectionId);
+
+    void goToSection(int sectionId, Bundle args);
+
+    void goToSection(int sectionId, boolean toBackStack);
+
+    void goToSection(int sectionId, Bundle args, boolean toBackStack);
 
 }
